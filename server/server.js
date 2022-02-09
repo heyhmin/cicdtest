6d755536192687a94773fcb9d4ce2c132deeb05f
EC2 Default User <ec2-user@ip-172-31-19-252.ap-northeast-2.compute.internal>
2022-02-09 07:24:43 +0000

    change position express json

--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ app.use(expressSession({
     proxy: true,
     saveUninitialized: false, cookie: { maxAge: 60000 * 60 }
 }));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
